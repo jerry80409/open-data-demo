450ed3f296c079d1cc9767fccb38e01c6d7d9063
Jerry Lai <jerry@urad.com.tw>
2017-02-10 17:55:23 +0800

    add Google Trends HotItems

--- src/main/java/com/example/services/google/trends/GoogleTrendsService.java
+++ src/main/java/com/example/services/google/trends/GoogleTrendsService.java
@@ -1,11 +1,13 @@
 package com.example.services.google.trends;
 
 import com.example.services.google.trends.entities.KeyWordsResponse;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-
-import java.util.HashMap;
-import java.util.List;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 /**
  * Created by jerry on 2017/2/7.
@@ -13,10 +15,25 @@ import java.util.List;
 public interface GoogleTrendsService {
 
     /**
-     * E.g. id=2172797
-     *
      * @return
      */
     @GET("visualize/internal/data")
     Call<KeyWordsResponse> trendsOfKeyWords();
+
+    /**
+     * curl 'https://www.google.com.tw/trends/hottrends/hotItems'
+     * -H 'Referer: https://www.google.com.tw/trends/hottrends'
+     * -H 'Origin: https://www.google.com.tw'
+     * -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+     * -H 'Content-Type: application/x-www-form-urlencoded;charset=UTF-8'
+     * --data 'ajax=1&pn=p12&htd&htv=l' --compressed
+     */
+    @Headers("Referer: https://www.google.com.tw/trends/hottrends")
+    @FormUrlEncoded
+    @POST("hotItems")
+    Call<ResponseBody> trendsOfHotItems(@Field("ajax") String ajax,
+                                        @Field("pn") String pn,
+                                        @Field("htd") String htd,
+                                        @Field("htv") String htv);
+
 }
--- src/test/java/com/example/services/google/trends/GoogleTrendsServiceTest.java
+++ src/test/java/com/example/services/google/trends/GoogleTrendsServiceTest.java
@@ -1,11 +1,13 @@
 package com.example.services.google.trends;
 
 import com.example.services.google.trends.entities.KeyWordsResponse;
+import okhttp3.ResponseBody;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
@@ -28,4 +30,21 @@ public class GoogleTrendsServiceTest {
         Assert.assertEquals(200, response.code());
     }
 
+    /**
+     * curl 'https://www.google.com.tw/trends/hottrends/hotItems'
+     * -H 'Referer: https://www.google.com.tw/trends/hottrends'
+     * -H 'Origin: https://www.google.com.tw'
+     * -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+     * -H 'Content-Type: application/x-www-form-urlencoded;charset=UTF-8'
+     * --data 'ajax=1&pn=p12&htd&htv=l' --compressed
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testGetTrendsOfHotItems() throws IOException {
+        Response<ResponseBody> response = googleTrendsService.trendsOfHotItems("1", "p12", "", "l").execute();
+        System.out.println(response.body());
+        Assert.assertEquals(200, response.code());
+//        Assert.assertEquals(200, responsecode());
+    }
 }
